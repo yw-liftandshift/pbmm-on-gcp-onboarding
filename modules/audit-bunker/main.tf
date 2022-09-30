634bb021eb2c4e3b8d92fb932ffd6ec2e1dd0516
uday.kakkar <git-uday.kakkar.yoppworks.com>
2022-09-30 00:15:05 +0000

    testing

--- modules/audit-bunker/main.tf
+++ modules/audit-bunker/main.tf
@@ -67,7 +67,8 @@ resource "google_logging_billing_account_sink" "billing-sink" {
   # Can export to pubsub, cloud storage, or bigquery
   destination = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
   depends_on = [
-    google_storage_bucket.log-bucket
+    google_storage_bucket.log-bucket,
+    google_project_iam_binding.log-writer
   ]
 }
 
