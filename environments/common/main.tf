49ba87da98b7e7040123bcff7ab6eeea13420214
uday.kakkar <git-uday.kakkar.yoppworks.com>
2022-10-07 18:47:40 +0000

    testing FW policy

--- environments/common/main.tf
+++ environments/common/main.tf
@@ -71,7 +71,8 @@ module "core-folders" {
 
 module "folder-firewall-policy" {
   source          = "../../modules/firewall-policy-folders"
-  folder_id = folders/521477516733 #module.core-folders.ids["folders_map_2_levels"]["ProdNetworking"].id
+  folder_id = folders/521477516733
+  #module.core-folders.ids["folders_map_2_levels"]["ProdNetworking"].id
  #parent           = module.core-folders.ids["ProdNetworking"]
   #names           = var.policy_folders.names
   firewall_policy_factory = {
@@ -80,7 +81,8 @@ module "folder-firewall-policy" {
     rules_file  = "data/rules.yaml"
   }
   firewall_policy_association = {
-    factory-policy = folders/521477516733 #module.core-folders.ids["folders_map_2_levels"]["ProdNetworking"].id
+    factory-policy = folders/521477516733
+    #module.core-folders.ids["folders_map_2_levels"]["ProdNetworking"].id
   }
 }
 
