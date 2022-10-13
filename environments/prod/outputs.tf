0a373c50694a51ea2649d2af9d3ac45c8633dbd6
uday.kakkar <git-uday.kakkar.yoppworks.com>
2022-10-13 10:04:24 +0000

    adding subnet link

--- environments/prod/outputs.tf
+++ environments/prod/outputs.tf
@@ -1,11 +1,3 @@
 output "prod_network" {
   value = module.net-host-prj
 }
-
-output "access_context_manager_parent_id" {
-  value = module.access-context-manager.parent_id
-}
-
-output "folders_map_2_levels" {
-  value = module.core-folders.folders_map_2_levels
-}
